85b2fcb8f1b7249a26eb78c10f500fe472352438
Federico Cattini <b994@live.com>
2026-04-16 17:32:46 +0200

    Replaced manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="it">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+      </head>
       <body>
         <RootLayoutClient>
           {children}
